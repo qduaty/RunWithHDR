685690d4836a223da292f18304f4c70823c05257
Sebastian Olter <qduaty@gmail.com>
2025-01-18 21:53:03 +0100

    added AutoActions

--- RunWithHDR.cpp
+++ RunWithHDR.cpp
@@ -7,25 +7,20 @@ using tstring = std::basic_string<TCHAR>;
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
     core::SetGlobalHDR(true);
-    // Podziel lpCmdLine na argumenty
     tstring cmdLine(lpCmdLine);
 
-    // Znajdź pierwszą spację jako rozdzielenie ścieżki programu potomnego i jego argumentów
     size_t pos = cmdLine.find(' ');
     tstring programPath = cmdLine.substr(0, pos);
     tstring programArgs = (pos == tstring::npos) ? _T("") : cmdLine.substr(pos + 1);
 
-    // Struktura STARTUPINFO
     STARTUPINFO si = { sizeof(si) };
     si.dwFlags = STARTF_USESHOWWINDOW;
-    si.wShowWindow = SW_SHOW; // Ukrywa konsolę
+    si.wShowWindow = SW_SHOW;
     si.dwFlags = 0;
     si.wShowWindow = 0;
 
-    // Struktura PROCESS_INFORMATION
     PROCESS_INFORMATION pi;
 
-    // Sklej ścieżkę programu potomnego z argumentami
     //tstring fullCmdLine = _T("\"") + programPath + _T("\" ") + programArgs;
     tstring fullCmdLine = programPath + _T(" ") + programArgs;
 
@@ -44,13 +39,11 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
     );
 
     if (success) {
-        // Zamknij uchwyty do procesu i wątku
         WaitForSingleObject(pi.hProcess, INFINITE);
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
     }
     else {
-        // Obsługa błędu
         MessageBox(nullptr, fullCmdLine.c_str(), _T("Nie dało się uruchomić programu"), MB_ICONERROR | MB_OK);
     }
     core::SetGlobalHDR(false);
